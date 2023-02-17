c934ee406b0e20ff9a9456b7a7d5018600992e21
KUNI <kunijeremy@gmail.com>
2023-02-17 11:39:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 function App() {
-  const name = "Brad";
   return (
     <div className="container">
       <Header />
+      <Tasks />
     </div>
   );
 }
